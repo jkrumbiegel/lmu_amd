ff46bf4148e64ba560b601f38f66af05faba1b85
Julius Krumbiegel <julius.krumbiegel@gmail.com>
2016-09-28 13:59:44 +0200

    added function help description

--- rotateCoordinates.m
+++ rotateCoordinates.m
@@ -1,4 +1,8 @@
 function rotCoords = rotateCoordinates(coords,degAngle)
+% ROTATECOORDINATES rotates coordinates by an angle given in degrees.
+% The pivot point is at [0,0]. If you want to rotate around another point
+% translate the coordinates by the appropriate amount first, rotate them,
+% and translate them back.
 
 rotMatrix = [cosd(degAngle), -sind(degAngle);...
              sind(degAngle),  cosd(degAngle)];
